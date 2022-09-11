35a2a53c69ae8acc4dfa25f2aba20f03560c8191
Rahul Agrawal <12agrawalrahul@gmail.com>
2022-09-11 23:40:53 +0530

    try6

--- Api/app.py
+++ Api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,render_template,request
+from flask import Flask,jsonify,render_template,request,send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import json
 from flask_cors import CORS,cross_origin
